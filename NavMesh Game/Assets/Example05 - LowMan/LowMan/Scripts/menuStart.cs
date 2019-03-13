4fc38fd492eaca230ae1877b23091b9667dcda3b
ZefFoster <zephramfoster@gmail.com>
2019-03-12 22:01:08 -0600

    My NavMesh Game Project
    For Game Programming Class

---
+++ NavMesh Game/Assets/Example05 - LowMan/LowMan/Scripts/menuStart.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuStart : MonoBehaviour
+{
+    public void ChangeMenuScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
